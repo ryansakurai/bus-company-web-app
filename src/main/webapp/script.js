90b83c74e1ebd60f098ebdc41158bc9f7f8367ab
Ryan Sakurai <ryansakurai03@gmail.com>
2023-12-11 06:11:51 -0300

    refatora cadasto no JS

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,35 +1,29 @@
-const parsePontosParada = function(pontosParadaString) {
-    const pontosDeParadaStr = pontosParadaString.split(';');
-    
-    const pontosDeParadaObj = [];
-    for(let pontodeParadaStr in pontosDeParadaStr) {
-        let tupla = pontodeParadaStr.split(',');
-        if(tupla.length === 2) {
-            pontosDeParadaObj.push({
-                "id": parseInt(tupla[0]),
-                "nome": tupla[1]
-            });
-        }
+const parsePontosDeParada = function(stringBruta) {
+    const listaDeStrings = stringBruta.split(';');
+    const listaDeObj = [];
+    for(let pontoStr in listaDeStrings) {
+        const tupla = pontoStr.split(',');
+        listaDeObj.push({
+            "endereco": tupla[0],
+            "regiao": tupla[1]
+        });
     }
-
-    return pontosDeParadaObj;
+    return listaDeObj;
 }
 
-const enviarFormulario = function() {
-    console.log("Chamado");
-    // Coletar dados do formulário
-    var formData = {
-        "id": parseInt($("#id").val()),
+const cadastrarLinha = function() {
+    const dado = [{
+        "id": parseInt( $("#id").val() ),
         "descricao": $("#descricao").val(),
         "terminal": $("#terminal").val(),
-        "pontosParada": parsePontosParada($("#pontosParada").val())
-    };
+        "usuariosDiarios": parseInt( $("#usuariosDiarios").val() ),
+        "itinerario": parsePontosDeParada( $("#itinerario").val() )
+    }];
 
-    // Enviar dados via AJAX
     $.ajax({
         type: "POST",
-        url: "/cadastroDeItinerario",
-        data: JSON.stringify(yourData),
+        url: "/cadastrar",
+        data: JSON.stringify(dado),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(data){
