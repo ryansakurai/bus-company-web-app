02f001b1aebafb855dcd78292344586fc4f674cb
Ryan Sakurai <ryansakurai03@gmail.com>
2023-12-12 15:24:30 -0300

    refatora rotas_itinerario.js

--- src/main/webapp/rotas_itinerario.js
+++ src/main/webapp/rotas_itinerario.js
@@ -1,42 +1,42 @@
-const exibirPorRegiao = function (regiao) {
-    $.getJSON(
-        'http://localhost:8080/mp1/recuperarPorRegiao',
-        { regiao: regiao },
-        function (data) {
-            console.log(data);
-            exibirItinerariosRegiao(data);
+const inserirHTML = function(listaLinhas) {
+    const div = $("#itinerario_div");
+    div.empty();
+
+    $.each(
+        listaLinhas,
+        function(idx, linha) {
+            const itinerario = linha.itinerario;
+            const card = $(`<div class="home_linhas">`);
+            card.append(`<h1 class="home_titulo">${linha.id} - ${linha.descricao}</h1>`);
+            card.append(`<p class="home_subtitulo">Ponto de Saída:</p>`);
+            card.append(`<p class="home_info">${linha.terminal}</p>`);
+            card.append(`<p class="home_subtitulo">Pontos:</p>`);
+            for(let i in itinerario)
+                card.append(`<p class="home_info">${itinerario[i].endereco}</p>`);
+            div.append(card);
         }
     );
 }
 
+const exibirPorRegiao = function(regiao) {
+    $.getJSON(
+        `recuperarPorRegiao`,
+        {regiao: regiao},
+        function(listaLinhas, status) {
+            console.log(status);
+            inserirHTML(listaLinhas);
+        }
+    );
+}
+
+
 $(document).ready(
-    function () {
+    function() {
         $('#carregar').click(
-            function () {
+            function() {
                 const regiao = $('#regiao').val();
                 exibirPorRegiao(regiao);
             }
         );
     }
 );
-
-function exibirItinerariosRegiao(textoItinerarios) {
-    var div = $("#itinerario_div");
-    div.empty();
-
-    $.each(textoItinerarios, function (index, linha) {
-        var itinerario = linha['itinerario'];
-        var card = $('<div class="home_linhas">');
-        card.append('<h1 class="home_titulo">' + linha['id'] + ' - ' + linha['descricao'] + '</h1>');
-        card.append('<p class="home_subtitulo">Ponto de Saída:</p>');
-        card.append('<p class="home_info">' + linha['terminal'] + '</p>');
-        card.append('<p class="home_subtitulo">Pontos:</p>');
-        for (let i = 0; i < itinerario.length; i++) {
-            card.append('<p class="home_info">' + itinerario[i].endereco + '</p>');
-        }
-        //card.append('<p class="home_info">' + itinerario[itinerario.length - 1].endereco + '</p>');
-        div.append(card);
-
-    }
-    );
-}
