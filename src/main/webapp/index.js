d753471cd812d684927a7afb3d644cc11ae0bc57
Ryan Sakurai <ryansakurai03@gmail.com>
2023-12-12 14:14:43 -0300

    refatora index.js

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,31 +1,31 @@
-const exibirMaisUsadas = function () {
+const inserirHTML = function(listaLinhas) {
+    const div = $(`#home_home`);
+    div.empty();
+
+    $.each(
+        listaLinhas,
+        function(idx, linha) {
+            const itinerario = linha.itinerario;
+            const card = $(`<div class="home_linhas">`);
+            card.append(`<h1 class="home_titulo">${linha.id} - ${linha.descricao}</h1>`);
+            card.append(`<p class="home_subtitulo">Ponto de Saída:</p>`);
+            card.append(`<p class="home_info">${linha.terminal}</p>`);
+            card.append(`<p class="home_subtitulo">Ponto Final:</p>`);
+            card.append(`<p class="home_info">${itinerario[itinerario.length-1].endereco}</p>`);
+            div.append(card);
+        }
+    );
+}
+
+const exibirMaisUsadas = function() {
     $.getJSON(
-        'http://localhost:8080/mp1/recuperarMaisUsadas',
-        { quantidade: 8 },
-        function (data) {
-            exibirItinerarios(data);
+        `recuperarMaisUsadas`,
+        {quantidade: 8},
+        function(linhasLista, status) {
+            console.log(status);
+            inserirHTML(linhasLista);
         }
     );
 }
 
 $(document).ready(exibirMaisUsadas);
-
-
-function exibirItinerarios(textoItinerarios) {
-    var div = $("#home_home");
-    div.empty();
-
-    $.each(textoItinerarios, function (index, linha) {
-        var itinerario = linha['itinerario'];
-        var card = $('<div class="home_linhas">');
-        card.append('<h1 class="home_titulo">' + linha['id'] + ' - ' + linha['descricao'] + '</h1>');
-        card.append('<p class="home_subtitulo">Ponto de Saída:</p>');
-        card.append('<p class="home_info">' + linha['terminal'] + '</p>');
-        card.append('<p class="home_subtitulo">Ponto Final:</p>');
-        card.append('<p class="home_info">' + itinerario[itinerario.length - 1].endereco + '</p>');
-        div.append(card);
-
-    }
-    );
-}
-
